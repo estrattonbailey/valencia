ceaf275e3e8692dd689f5ea4b337879db0f524ea
estrattonbailey <ericstrattonbailey@gmail.com>
2016-09-27 11:49:24 -0400

    Build initial version

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 import jsonp from 'micro-jsonp'
 
-export default ({ token, posts }, callback) => {
-  return
-  token && jsonp(`https://api.instagram.com/v1/users/self/media/recent/?access_token=${token}&count=${posts || 1}`, {
-    response: (err, data) => {
-      if (err) console.log(err)
-      callback(data)
+export default ({ token, count }, cb) => {
+  const data = {
+    images: [],
+    next: () => console.log('Awaiting response...'),
+    res: {}
+  }
+
+  const response = (err, res) => {
+    if (err) console.log(err)
+
+    data.images = res.data
+    data.next = next(res.pagination.next_url)
+    data.res = res
+
+    cb(data)
+
+    return {
+      images: data.images,
+      next: data.next,
+      res
     }
+  }
+
+  const next = url => done => get(url, done ? (err, res) => {
+    let data = response(err, res)
+    done(data)
+  } : response)
+
+  const get = (url, done) => jsonp(url, {
+    response: done 
   })
+
+  get(`https://api.instagram.com/v1/users/self/media/recent/?access_token=${token}&count=${count || 1}`, response)
+
+  return data 
 }
