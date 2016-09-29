776b26ff7b0c69ae2c33b1dd566cbdea7ee280c2
estrattonbailey <ericstrattonbailey@gmail.com>
2016-09-28 20:10:16 -0400

    Add default cb

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import jsonp from 'micro-jsonp'
 
-export default ({ token, count }, cb) => {
+export default ({ token, count }, cb = () => {}) => {
   const data = {
     images: [],
     next: () => console.log('Awaiting response...'),
@@ -8,7 +8,7 @@ export default ({ token, count }, cb) => {
   }
 
   const response = (err, res) => {
-    if (err) console.log(err)
+    if (err) return console.log(err)
 
     data.images = res.data
     data.next = next(res.pagination.next_url)
@@ -23,9 +23,7 @@ export default ({ token, count }, cb) => {
 
   const next = url => done => get(url, (err, res) => done ? done(response(err, res)) : cb(response(err, res)))
 
-  const get = (url, done) => jsonp(url, {
-    response: done 
-  })
+  const get = (url, done) => jsonp(url, { response: done })
 
   get(`https://api.instagram.com/v1/users/self/media/recent/?access_token=${token}&count=${count || 1}`, response)
 
